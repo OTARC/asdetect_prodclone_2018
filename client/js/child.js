022c3028dcf50126f4b0d3091c28a9b652a126df
Mike Burnside <mburnside@salesforce.com>
2015-07-30 16:04:28 +1000

    first cut template and controller for child list

---
+++ client/js/child.js
@@ -0,0 +1,44 @@
+angular.module('nibs.child', ['openfb', 'nibs.status', 'nibs.activity', 'nibs.wallet'])
+
+    // Routes
+    .config(function ($stateProvider) {
+
+        $stateProvider
+
+            .state('app.child', {
+                url: "/child",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/child-list.html",
+                        controller: "ChildListCtrl"
+                    }
+                }
+            })
+
+
+           
+
+    })
+
+    // Services
+    .factory('Child', function ($http, $rootScope) {
+        return {
+            all: function() {
+                return $http.get($rootScope.server.url + '/child');
+            },
+            get: function(offerId) {
+                return $http.get($rootScope.server.url + '/child/' + childId);
+            }
+        };
+    })
+
+    //Controllers
+    .controller('ChildListCtrl', function ($scope, $rootScope, $ionicPopup, $ionicModal, Offer, User) {
+        Offer.all().success(function(children) {
+            $scope.children = children;
+        });
+
+       
+    })
+
+
