a06f0206d41b670db44a50c37109766b0fb2b486
Mike Burnside <mburnside@salesforce.com>
2015-08-03 16:36:39 +1000

    started consultation, added externalchildid to mch_child_Asdetect__c

--- server/child.js
+++ server/child.js
@@ -4,12 +4,12 @@ var db = require('./pghelper'),
 
 function findById(id) {
     // Retrieve offer either by Salesforce id or Postgress id
-    return db.query('select id,sfId,Childs_Initials__c as childsInitials,Birthdate__c as birthdate,gender__c as gender ,Child_currently_at_risk__c as currentlyAtRisk,child__c as asdetect_contact from salesforce.mch_child_Asdetect__c WHERE ' + (isNaN(id) ? 'sfId' : 'id') + '=$1', [id], true);
+    return db.query('select id,sfId,Childs_Initials__c as childsInitials,Birthdate__c as birthdate,gender__c as gender ,Child_currently_at_risk__c as currentlyAtRisk,child__c as asdetect_contact,externalchildid__c as externalchildid from salesforce.mch_child_Asdetect__c WHERE ' + (isNaN(id) ? 'sfId' : 'id') + '=$1', [id], true);
 };
 
 function getAll(req, res, next) { 
     var externalUserId = req.externalUserId;
-    db.query("select id,sfId,Childs_Initials__c as childsInitials,Birthdate__c as birthdate,gender__c as gender ,Child_currently_at_risk__c as currentlyAtRisk,child__c as asdetect_contact from salesforce.mch_child_Asdetect__c where asdetect_contact__c__loyaltyid__c=$1 LIMIT $2", [externalUserId,20])       
+    db.query("select id,sfId,Childs_Initials__c as childsInitials,Birthdate__c as birthdate,gender__c as gender ,Child_currently_at_risk__c as currentlyAtRisk,child__c as asdetect_contact,externalchildid__c as externalchildid from salesforce.mch_child_Asdetect__c where asdetect_contact__c__loyaltyid__c=$1 LIMIT $2", [externalUserId,20])       
         .then(function (child) {
             return res.send(JSON.stringify(child));
         })
