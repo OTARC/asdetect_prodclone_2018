9b919ac82141a302725f0d4bb48f4d85dab00438
Mike Burnside <mburnside@salesforce.com>
2015-08-04 17:43:21 +1000

    added consultation and basic method to server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var express = require('express'),
     activities = require('./server/activities'),
     //ASDECTECT
     child=require('./server/child');
-    child=require('./server/consultation');
+    consultation=require('./server/consultation');
 
     app = express();
 
