6fb9a7ec46093b75e477e986a10fb52d845abc4e
Mike Burnside <mburnside@salesforce.com>
2015-08-05 13:15:10 +1000

    added assessment tempaltes and routes

--- server/assessment.js
+++ server/assessment.js
@@ -9,8 +9,7 @@ function findById(id) {
 
 function getAll(req, res, next) { 
     var externalUserId = req.externalUserId;
-    db.query('select c.id,c.sfid,c.name, c.consultation_date__c as consultationdate,c.record_type__c as recordtype,c.mch_child_asdetect__r__externalchildid__c as externalchildid, c.mch_child_asdetect__c,c.at_risk__c as atrisk from salesforce.consultation_asdetect__c c,salesforce.mch_child_asdetect__c m where
-c.mch_child_asdetect__r__externalchildid__c=m.externalchildid__c and m.asdetect_contact__c__loyaltyid__c=$1',[externalUserId]) 
+    db.query("select c.id,c.sfid,c.name, c.consultation_date__c as consultationdate,c.record_type__c as recordtype,c.mch_child_asdetect__r__externalchildid__c as externalchildid, c.mch_child_asdetect__c,c.at_risk__c as atrisk from salesforce.consultation_asdetect__c c,salesforce.mch_child_asdetect__c m where c.mch_child_asdetect__r__externalchildid__c=m.externalchildid__c and m.asdetect_contact__c__loyaltyid__c=$1",[externalUserId]) 
         .then(function (assessment) {
             return res.send(JSON.stringify(assessment));
         })
