f1cf4d116ae5b17d54627ad009318b52ab1e136d
Mike Burnside <mburnside@salesforce.com>
2015-08-19 11:14:35 +1000

    added a simple interaction capability

---
+++ client/js/interaction.js
@@ -0,0 +1,48 @@
+angular.module('nibs.interaction', [])
+
+    // Routes
+    .config(function ($stateProvider) {
+
+        $stateProvider
+
+            .state('app.interaction', {
+                url: "/interaction",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/interaction-list.html",
+                        controller: "InteractionCtrl"
+                    }
+                }
+            })
+
+    })
+
+    // Services
+    .factory('Interaction', function ($http, $rootScope) {
+        return {
+            all: function() {
+                return $http.get($rootScope.server.url + '/interactions');
+            },
+            create: function(activity) {
+                return $http.post($rootScope.server.url + '/interactions/', activity);
+            },
+            deleteAll: function() {
+                return $http.delete($rootScope.server.url + '/interactions');
+            }
+        };
+    })
+
+    //Controllers
+    .controller('InteractionCtrl', function ($scope, $state, Activity) {
+        Activity.all().success(function(interactions) {
+            $scope.interactions = interactions
+        });
+
+        $scope.doRefresh = function() {
+            Activity.all().success(function(interactions) {
+                $scope.interactions = interactions;
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
+
+    });
