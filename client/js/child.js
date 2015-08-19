d3c4c94e883e87cccfe7fbdc4f5cdd1ce4a86463
Mike Burnside <mburnside@salesforce.com>
2015-08-19 14:00:51 +1000

    added interaction list to menu and created a template

--- client/js/child.js
+++ client/js/child.js
@@ -146,7 +146,7 @@ angular.module('nibs.child', ['openfb', 'nibs.status', 'nibs.activity','nibs.int
                 $ionicPopup.alert({title: 'Thank You', content: 'Child record created.'});
             });
 
-             Interaction.create({type__c: "Child: " + $scope.child.childs_initials__c"+  added", description__c:"from Angular module."})
+             Interaction.create({type__c: "Child: " + $scope.child.childs_initials__c+"+  added", description__c:"from Angular module."})
                 .success(function(status) {
                     console.log('Interaction recorded.');
                 });
