572e8d99d4792883b0fbf4ec95ba2230fec60e8e
Mike Burnside <mburnside@salesforce.com>
2015-10-09 16:36:32 +1100

    all log in/out is now server based n node.js auth

--- server/auth.js
+++ server/auth.js
@@ -128,8 +128,8 @@ function login(req, res, next) {
             comparePassword(creds.password__c, user.password__c, function (err, match) {
                 if (err) return next(err);
                 if (match) {  
-                     logUserInteraction(user.externaluserid,'Logged In','Node.js auth')   
-                      
+                     //logUserInteraction(user.externaluserid,'Logged In','Node.js auth')   
+
                      .then (cleanupAccessTokens(user))
 
                      .then (createAccessToken(user))
