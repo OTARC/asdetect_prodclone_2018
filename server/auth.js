8d74bd8bc743f356277461740241e668f8c621eb
Mike Burnside <mburnside@salesforce.com>
2016-02-16 14:59:21 +1100

    modified username/passwordwarning to say letters and numbers and spaces

--- server/auth.js
+++ server/auth.js
@@ -223,10 +223,10 @@ function signup(req, res, next) {
         return res.send(400, "Invalid email address");
     }
     if (!validator.isLength(user.firstname__c, 1) || !validator.isAlphanumeric(user.firstname__c)) {
-        return res.send(400, "First name must be letters and numbers only and be at least one character");
+        return res.send(400, "First name must be alphanumeric and not contain spaces");
     }
     if (!validator.isLength(user.lastname__c, 1) || !validator.isAlphanumeric(user.lastname__c)) {
-        return res.send(400, "Last name must be letters and numbers only and be at least one character");
+        return res.send(400, "Last name must be alphanumeric and not contain spaces");
     }
     if (!validator.isLength(user.password__c, 4)) {
         return res.send(400, "Password must be at least 4 characters");
