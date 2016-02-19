3f42d6bd0981b76fb7f1515d9a990983eaa19edc
Mike Burnside <mburnside@salesforce.com>
2016-02-19 22:36:27 +1100

    removed a password in the clear from console output

--- server/auth.js
+++ server/auth.js
@@ -119,7 +119,7 @@ function login(req, res, next) {
     var creds = req.body;
 
     winston.info('login(): '+creds.email__c);
-    console.log(creds);
+    //console.log(creds);
 
     var parser = new UAParser();
     var ua = req.headers['user-agent'];
