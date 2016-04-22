cd3ef5082ff485b53127907112f946a3d6776f0a
Lachlan James Burnside <l.burnside@latrobe.edu.au>
2016-04-22 13:04:07 +1000

    Added 'country' in signup() debug message - trying to figure out why the country field isn't populating properly

--- server/auth.js
+++ server/auth.js
@@ -213,7 +213,8 @@ function logout(req, res, next) {
 function signup(req, res, next) {
 
     var user = req.body;
-    winston.info('signup(): email=['+user.email__c+'] firstname=['+user.firstname__c+'] lastname=['+user.lastname__c+']');
+    winston.info('signup(): email=['+user.email__c+'] firstname=['+user.firstname__c+
+    		'] lastname=['+user.lastname__c+'] country='+user.country__c);
 
     //console.log(user);
 
