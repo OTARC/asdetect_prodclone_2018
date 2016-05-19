ff40321124c15674db368c4c47c1393da7646a56
Lachlan James Burnside <l.burnside@latrobe.edu.au>
2016-05-19 17:38:06 +1000

    REST endpoint on Contact object is 1.11

--- server/auth.js
+++ server/auth.js
@@ -312,7 +312,7 @@ function createUser(user, password) {
         externalUserId=uuid.v4();
 
     db.query('INSERT INTO latrobeasdetect.asdetect_contact__c (email__c, password__c, firstname__c, lastname__c, country__c, loyaltyid__c,rest_endpoint_version__c) VALUES ($1, $2, $3, $4, $5, $6,$7) RETURNING id, firstname__c, lastname__c, email__c, loyaltyid__c as externalUserId',
-        [user.email__c, password, user.firstname__c, user.lastname__c, user.country__c, externalUserId,'1.1'], true)
+        [user.email__c, password, user.firstname__c, user.lastname__c, user.country__c, externalUserId, '1.11'], true)
         .then(function (insertedUser) {
             deferred.resolve(insertedUser);
         })
