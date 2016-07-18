1a21405d87429e4093345ba0e924a7eb1d897d86
Mike Burnside <mburnside@salesforce.com>
2016-07-18 11:18:50 +1000

    temp fix to login problem

--- server/auth.js
+++ server/auth.js
@@ -164,7 +164,7 @@ function login(req, res, next) {
                     
                     //Code has been changed
                     console.log("attempting to update contacts where email__c = $1", [user.email__c]);
-                    db.query('update latrobeasdetect.asdetect_contact__c SET REST_endpoint_version__c=$1 WHERE email__c=$2', [endpoint, user.email__c]);
+                    //db.query('update latrobeasdetect.asdetect_contact__c SET REST_endpoint_version__c=$1 WHERE email__c=$2', [endpoint, user.email__c]);
                     
                     logUserInteraction(user.externaluserid,'Logged In','Node.js auth',os)    
                     .then             
