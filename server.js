cc14c3cfd7542c0a92b21bbc432ca192a5c8f108
ASDetect - Olga Tennison Autism Research Centre <36247257+OTARC@users.noreply.github.com>
2018-02-13 11:31:33 +1100

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var express = require('express'),
 
 app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 app.use(compression());
 app.use(bodyParser({
